03ceac4310931951f180db4de25d9d0fe9fa4485
Alan Pecenkovic <alanquad@yahoo.com>
2023-09-14 14:36:57 +0200

    flexbox layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { Box, Button, Flex, HStack, Heading, Spacer, Text } from "@chakra-ui/react";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <Flex as="nav" p="100px" alignItems="center">
+      <Heading as="h1" size="4xl">
+        Sninobi missions
+      </Heading>
+      <Spacer></Spacer>
+
+      <HStack spacing="20px">
+        <Box bg="gray.200" p="10px">
+          Kakuzu
+        </Box>
+        <Text>kakuzu@hiddenvillage.dev</Text>
+        <Button colorScheme="purple">Logout</Button>
+      </HStack>
+    </Flex>
+    // <Flex bg="gray.200" justify="space-between" wrap="wrap" gap="2">
+    //   <Box w="150px" h="50px" bg="red">
+    //     1
+    //   </Box>
+    //   <Box w="150px" h="50px" bg="blue">
+    //     2
+    //   </Box>
+    //   <Box w="150px" h="50px" bg="green" flexGrow="1">
+    //     3
+    //   </Box>
+    //   <Box w="150px" h="50px" bg="yellow" flexGrow="2">
+    //     4
+    //   </Box>
+    // </Flex>
+  );
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,21 +15,22 @@ export default function Dashboard() {
   };
 
   return (
-    <Container as="section" maxW="lg" py={"10px"} bgColor={"beige"} borderRadius={20}>
+    <Container as="section" maxW="4x1" py={"20px"}>
       <Heading m={"30px"} p={"10px"}>
         Chakra UI components
       </Heading>
-      <Text ml={"30px"}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore ut inventore
-        unde dolor molestiae hic earum quidem dolore quos maxime veritatis, iste
-        reprehenderit eaque consequatur provident perspiciatis nobis alias ratione?
+      <Text ml={"30px"} fontWeight={"bold"}>
+        Chakra UI is a simple, modular and accessible component library that gives you
+        the building blocks you need to build your React applications.
       </Text>
       <Text ml={"30px"} color={"blue.400"} fontWeight={"bold"}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore ut inventore
-        unde dolor molestiae hic earum quidem dolore quos maxime veritatis, iste
-        reprehenderit eaque consequatur provident perspiciatis nobis alias ratione?
+        Chakra UI is established on principles that keep its components fairly
+        consistent. Understanding these concepts will help you better contribute to
+        Chakra UI. Our goal is to design simple, composable components that cater to
+        real-life UI design problems. In order to do that, we developed a set of
+        principles that help us always be on that path.
       </Text>
-      <Box m={"30px"} p={"20px"} bg={"orange"}>
+      <Box m="30px" p="20px" bg="orange">
         <Text color={"white"}>This is inside a box</Text>
       </Box>
 
